7e82aef61e9187779ae7b492bb0323eadddb0a49
Sherlin-S <sherlinkmr@gmail.com>
2025-06-09 09:50:33 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "www.logopeople.in",
+      "interhospi.com",
+      "www.shutterstock.com",
+      "www.ripplesiot.com",
+    ],
+  },
 };
 
 export default nextConfig;
